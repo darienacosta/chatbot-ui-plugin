65f3e982d2ead1e8a61a4438ef4b735ef1b76d93
darienacosta <darien.01@gmail.com>
2023-07-12 23:34:41 -0400

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -340,16 +340,16 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href="https://coverwhale-chatgpt4.vercel.app/"
           target="_blank"
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          Cover Whale ChatGPT4 Paid API Portal
         </a>
         .{' '}
         {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          "Queries are covered by OpenAI’s API data usage policy. OpenAI will not use API data to train their models and purge queries after 30 days.",
         )}
       </div>
     </div>
