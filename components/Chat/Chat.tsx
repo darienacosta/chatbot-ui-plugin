88719a3471a3a95930dff9c8287ffc91ec984d59
darienacosta <darien.01@gmail.com>
2023-07-16 18:58:19 -0400

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -333,7 +333,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            CoverWhale Paid API ChatGPT4 Portal
+            🐳CoverWhale Paid API ChatGPT4 Portal
           </div>
           <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
@@ -384,7 +384,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'CoverWhale Paid API ChatGPT4 Portal'
+                      '🐳CoverWhale Paid API ChatGPT4 Portal'
                     )}
                   </div>
 
